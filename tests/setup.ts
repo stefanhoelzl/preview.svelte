56b5d2df8bb1d434069269af0b9a4e3fc106132a
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2024-04-16 22:01:40 +0200

    [internal] preparation for preview component tests.

---
+++ tests/setup.ts
@@ -0,0 +1,35 @@
+import { beforeAll } from "vitest";
+import { compile } from "svelte/compiler";
+import "@testing-library/jest-dom/vitest";
+import { withFile } from "tmp-promise";
+import { promisify } from "node:util";
+import fs from "node:fs";
+
+const mkdir = promisify(fs.mkdir);
+const writeFile = promisify(fs.writeFile);
+
+const TmpDir = ".tmp";
+
+beforeAll(async () => {
+  await mkdir(TmpDir, { recursive: true });
+});
+
+export async function withTemporaryModule<R>(
+  code: string,
+  cb: (module: string) => Promise<R>,
+) {
+  const { js } = compile(code, { dev: true });
+  return await withFile(
+    async ({ path }) => {
+      await writeFile(path, js.code);
+      return await cb(path);
+    },
+    { postfix: ".js", tmpdir: TmpDir },
+  );
+}
+
+export async function createDynamicComponent(code: string) {
+  return await withTemporaryModule(code, async (path) => {
+    return (await import(path)).default;
+  });
+}
